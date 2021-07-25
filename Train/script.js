2d6f948529a0cc7904db05ea0576ba9db852791a
Sergo <sergey199014@mail.ru>
2021-07-25 20:19:08 +0700

    Добавил условия

--- Train/script.js
+++ Train/script.js
@@ -10,13 +10,27 @@ genres: [],
 privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?','');
 
-      personalMuvieDB.movies[a] =b;
-      personalMuvieDB.movies[c] =d;
+      for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?','');
+              if (a!=null && b!=null && a!='' && b!='' && a.length <50) {
+                personalMuvieDB.movies[a] =b;
+                console.log('done');
+              } else {
+                  console.log('error');
+                  i --;
+              }       
+      }
+      if (personalMuvieDB.count < 10) {
+          console.log('Просмотрено довольно мало фильмов');
+      } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count < 30) {
+          console.log('Вы хороший зритель!');
+      } else if (personalMuvieDB.count >= 30) {
+          console.log('Вы киноман!');
+      } else {
+          console.log('Error');
+      }
 
       console.log(personalMuvieDB);
 

b6c14f6b19f74a9c5d4b1de7e5f0f014e8673fea
Sergo <sergey199014@mail.ru>
2021-08-06 22:18:27 +0700

    new progect

--- Train/script.js
+++ Train/script.js
@@ -1,83 +1,29 @@
 'use strict';
 
-const personalMuvieDB = {
-count: 0,
-movies: {},
-actors: {},
-genres: [],
-privat: false,
-start: function() {
-    personalMuvieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const box = document.getElementById('box'),
+btns = document.getElementsByTagName('button'),
+circles = document.getElementsByClassName('circle'),
+hearts = document.querySelectorAll('.heart'),
+oneHeart = document.querySelector('.heart'),
+wrapper = document.querySelector('.wrapper');
 
-    while (personalMuvieDB.count == '' || personalMuvieDB.count == null || isNaN(personalMuvieDB.count)) {
-        personalMuvieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-},
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-rememberMyFilms: function  (){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?','');
-              if (a!=null && b!=null && a!='' && b!='' && a.length <50) {
-                personalMuvieDB.movies[a] =b;
-                console.log('done');
-              } else {
-                  console.log('error');
-                  i --;
-              }       
-      }
-},
+btns[2].style.borderRadius = '100%';
 
-detectPersonalLevel: function(){
-    if (personalMuvieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMuvieDB.count >= 10 && personalMuvieDB.count < 30) {
-        console.log('Вы хороший зритель!');
-    } else if (personalMuvieDB.count >= 30) {
-        console.log('Вы киноман!');
-    } else {
-        console.log('Error');
-    }
-  },
+box.style.cssText = 'background-color: red; width: 300px;';
 
-  showMyDB: function (hidden){
-    if (!hidden){
-        console.log(personalMuvieDB);
-    }
-},
-
-togleVisibleMyDB: function(){
-if (personalMuvieDB.privat) {
-    (personalMuvieDB.privat) = false;
-} else {
-    (personalMuvieDB.privat) = true;
-}
-
-},
-
-writeYourGenres: function() {
-    for (let i=1; i <=3; i++){
-        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-
-if (genre === '' || genre === null) {
-console.log('Вы ввели некорректные данные');
-i--;   
-} else {
-    personalMuvieDB[i - 1] = genre;
-}
-}
-
-personalMuvieDB.genres.forEach((item, i) => {
-console.log(`Любимый жанр ${i + 1} - это ${item}`);
+// // for (let i = 0; i < hearts.length; i++) {
+// //     hearts[i].style.backgroundColor = 'pink';
+// }
+hearts.forEach(item=>{
+item.style.backgroundColor = 'pink';
 });
-}
 
+const div =document.createElement('div');
 
-};
+div.classList.add('black');
+wrapper.append(div);
 
-
-
-
-
-      
-
+div.innerHTML = 'Hello World!';
